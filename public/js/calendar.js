809202c6408caa9bec594f51259b7f3eadce8b81
jmpark6846 <jmpark6846@gmail.com>
2018-04-14 13:54:08 +0900

    displayCalendar 매개변수로 초기값 지정; html에서 년,월 선택 추가

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -2,22 +2,15 @@ $(document).ready(function(){
     displayCalendar();
 });
 
-function displayCalendar(){
-    let now = new Date(),
-        currentYear = now.getFullYear(),
-        currentMonth = now.getMonth(),
-        currentDate = now.getDate(),
-        currentDay = now.getDay();
-
-    let html = "";
-
-    let endDates = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // 매 달 마지막 날짜
-    let firstDate = new Date(Date.UTC(currentYear, currentMonth, 1)), // 해당 달 첫째날
+function displayCalendar(year = new Date().getFullYear(), month = new Date().getMonth()){  
+    let html = "",
+        endDates = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31], // 매 달 마지막 날짜
+        firstDate = new Date(Date.UTC(year, month, 1)), // 해당 달 첫째날
         startDayOfWeek = 0, // 한 주의 시작은 일요일
-        columns = (currentMonth === 2) && (startDayOfWeek === firstDate.getDay()) ? 4 : 5, // 달력 전체 column 수 계산. 2월 중 달 첫째날 요일이 한 주 시작 요일과 같은 경우는 4줄, 다른 모든 경우는 5줄이다.
+        columns = (month === 2) && (startDayOfWeek === firstDate.getDay()) ? 4 : 5, // 달력 전체 column 수 계산. 2월 중 달 첫째날 요일이 한 주 시작 요일과 같은 경우는 4줄, 다른 모든 경우는 5줄이다.
         day=1;
 
-    html += '<div>' + (currentMonth+1) + '월</div>\
+    html += '<div>' + (month+1) + '월</div>\
             <table>\
             <tr>\
                 <td>일</td>\
@@ -39,7 +32,7 @@ function displayCalendar(){
         for(let j=0; j<7-space; j++){   // 2. 칸에 맞게 숫자를 출력한다.
             html += `<td>${day++}</td>`;
 
-            if(day > endDates[currentMonth]) break; // 3. 숫자가 endDates 보다 크면 루프 빠져나옴
+            if(day > endDates[month]) break; // 3. 숫자가 endDates 보다 크면 루프 빠져나옴
         }
 
         space=0;
